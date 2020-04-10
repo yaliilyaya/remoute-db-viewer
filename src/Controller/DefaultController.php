81e9fbc9ff8a9063f07910c83968f912589ff9ad
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-04-11 00:14:43 +0300

    mainPage.html.twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function index()
     {
+        return $this->render('mainPage.html.twig', [
 
+        ]);
     }
 }
