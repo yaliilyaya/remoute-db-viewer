c9b8904eb9062f29195766eed8f1653812d4af5b
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-04-28 03:15:56 +0300

    remoute-db-viewer-9 Настройка отображения полей в списке строк

--- src/Controller/DataBaseConnect/ConfigController.php
+++ src/Controller/DataBaseConnect/ConfigController.php
@@ -7,6 +7,7 @@ namespace App\Controller\DataBaseConnect;
 use App\Entity\RemoteDataBase;
 use App\Factory\ConnectionByDataBaseFactory;
 use App\Repository\DataBaseRepository;
+use App\Repository\RemoteTableColumnRepository;
 use App\Repository\RemoteTableRepository;
 use App\Service\SyncRemoteTableService;
 use Doctrine\DBAL\DBALException;
@@ -24,32 +25,68 @@ class ConfigController  extends AbstractController
      * @var ConnectionByDataBaseFactory
      */
     private $connectionByDataBaseFactory;
+    /**
+     * @var RemoteTableColumnRepository
+     */
+    private $remoteTableColumnRepository;
 
     public function __construct(
         RemoteTableRepository $remoteTableRepository,
+        RemoteTableColumnRepository $remoteTableColumnRepository,
         ConnectionByDataBaseFactory $connectionByDataBaseFactory
     ) {
 
         $this->remoteTableRepository = $remoteTableRepository;
         $this->connectionByDataBaseFactory = $connectionByDataBaseFactory;
+        $this->remoteTableColumnRepository = $remoteTableColumnRepository;
     }
 
     /**
-     * @Route("/config/column/{tableId}", name="configColumn")
+     * @Route("/config/table/{tableId}", name="configTable")
      * @param $tableId
      * @return Response
      * @throws DBALException
      */
-    public function configColumn($tableId)
+    public function configTable($tableId)
     {
+        $table = $this->remoteTableRepository->find($tableId);
 
+        return $this->render('config/table.html.twig', [
+            'table' => $table
+        ]);
+    }
+
+    /**
+     * @Route("/config/columns/{tableId}", name="configColumns")
+     * @param $tableId
+     * @return Response
+     * @throws DBALException
+     */
+    public function configColumns($tableId)
+    {
         $table = $this->remoteTableRepository->find($tableId);
 
         $connection = $this->connectionByDataBaseFactory->createConnection($table->getDatabase());
         $table->setConnection($connection);
 
-        return $this->render('config/column.html.twig', [
+        return $this->render('config/columns.html.twig', [
             'columns' => $table->getColumns()
         ]);
     }
+
+    /**
+     * @Route("/config/column/{columnId}", name="configColumn")
+     * @param $columnId
+     * @return Response
+     * @throws DBALException
+     */
+    public function configColumn($columnId)
+    {
+        $column = $this->remoteTableColumnRepository->find($columnId);
+
+        return $this->render('config/column.html.twig', [
+            'column' => $column
+        ]);
+    }
+
 }
