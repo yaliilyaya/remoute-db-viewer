9c9bc6b2c14d5bbbf6037e39d6bc1874e803421f
Ilya Didenko <didenko.i.a@effective-group.ru>
2020-04-29 00:36:15 +0300

    remoute-db-viewer-9 Настройка отображения полей в списке строк [edit view column]

--- src/Controller/Editor/RemoteColumnController.php
+++ src/Controller/Editor/RemoteColumnController.php
@@ -8,6 +8,7 @@ use App\Form\Type\RemoteTableType;
 use App\Repository\RemoteTableColumnRepository;
 use App\Repository\RemoteTableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,9 +33,6 @@ class RemoteColumnController  extends AbstractController
         $this->remoteTableColumnRepository = $remoteTableColumnRepository;
     }
 
-
-
-
     /**
      * @Route("/config/column/{columnId}", name="configColumn")
      * @param Request $request
@@ -56,12 +54,59 @@ class RemoteColumnController  extends AbstractController
             //return $this->redirect("/dataBase/list");
         }
 
-
         return $this->render('config/column.html.twig', [
             'form' => $form->createView(),
             'column' => $column,
             'edit' => true
         ]);
+    }
+
+    /**
+     * @Route("/config/column/enabledViewList/{columnId}/{enabled}", name="columnEnabledViewList")
+     * @param int $columnId
+     * @param string $enabled
+     * @return RedirectResponse
+     */
+    public function enabledViewList($columnId, $enabled)
+    {
+        $column = $this->remoteTableColumnRepository->find($columnId);
+        $column->setIsViewList($enabled === 'true');
+
+        $this->remoteTableColumnRepository->save($column);
+
+        return $this->redirect('/config/columns/' . $column->getTable()->getId());
+    }
+
+    /**
+     * @Route("/config/column/enabledViewDetail/{columnId}/{enabled}", name="columnEnabledViewDetail")
+     * @param int $columnId
+     * @param string $enabled
+     * @return RedirectResponse
+     */
+    public function enabledViewDetail(int $columnId, string $enabled)
+    {
+        $column = $this->remoteTableColumnRepository->find($columnId);
+        $column->setIsViewDetail($enabled === 'true');
+
+        $this->remoteTableColumnRepository->save($column);
+
+        return $this->redirect('/config/columns/' . $column->getTable()->getId());
+    }
+
+    /**
+     * @Route("/config/column/enabledViewPopup/{columnId}/{enabled}", name="columnEnabledViewPopup")
+     * @param int $columnId
+     * @param string $enabled
+     * @return RedirectResponse
+     */
+    public function enabledViewPopup($columnId, $enabled)
+    {
+        $column = $this->remoteTableColumnRepository->find($columnId);
+        $column->setIsViewPopup($enabled === 'true');
+
+        $this->remoteTableColumnRepository->save($column);
+
+        return $this->redirect('/config/columns/' . $column->getTable()->getId());
     }
 
 }
